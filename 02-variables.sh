b9db7060dee364681d4d0cc6f5220ee7808f26db
Lav-Devops <lavanyamatta1812@gmail.com>
2025-04-06 22:26:25 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "Jithu: Hi Lav, Good Evening"
-echo "Lav: Hi Jithu, Very good evening"
+PERSON1:Jithu
+PERSON2:lav
+
+echo "$PERSON1: Hi $PERSON2, Good Evening"
+echo "$PERSON2: Hi $PERSON1, Very good evening"
