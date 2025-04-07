3e516593fc0ae18de6dab53dcd73bd0bc630189e
Lav-Devops <lavanyamatta1812@gmail.com>
2025-04-07 12:05:28 +0530

    conditions script

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,22 +3,26 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE() {
 if [ $1 -ne 0 ]
 then
- echo -e "Installation $2 \e[31m failed"
+ echo -e "$R ERROR: $N Installation $2  $R failed $N"
 else
- echo -e "Installation $2 \e[32m success"
+ echo -e "Installation $2 $G success $N"
 fi
 }
 
 if [ $ID -ne 0 ]
 then
- echo -e " \e[31m Run with root user"
+ echo -e "$R Run with root user $N"
  exit 1
 else
- echo -e " \e[32m you are root user"
+ echo -e "$Y you are root user $N"
 fi
 
 yum install mysql -y &>> $LOGFILE
